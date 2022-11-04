0e93b97a4aaa38c87b5a3a73d78d49dadde4c8bd
letmp <le_tmp@gmx.de>
2022-11-04 14:05:27 +0100

    added globals.h for global constants added simple MAC-address-check to start mqtt broker only on one board added mDNS service and mDNS service discovery

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <TinyMqtt.h>   // https://github.com/hsaturn/TinyMqtt
+#include <ESPmDNS.h>
+#include <globals.h>
 
 /** 
   * Local broker that accept connections and two local clients
@@ -32,8 +34,8 @@
   *
   */
 
-const char *ssid     = "HiFiWiFi";
-const char *password = "";
+const char *ssid     = WIFI_SSID;
+const char *password = WIFI_PWD;
 
 std::string topic="sensor/temperature";
 
@@ -48,6 +50,8 @@ void onPublishA(const MqttClient* /* source */, const Topic& topic, const char* 
 void onPublishB(const MqttClient* /* source */, const Topic& topic, const char* payload, size_t /* length */)
 { Serial << "--> client B Received " << topic.c_str() << ", " << payload << endl; }
 
+bool startBroker = false;
+
 void setup()
 {
   Serial.begin(115200);
@@ -60,46 +64,94 @@ void setup()
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {   Serial << '-'; delay(500); }
+  while (WiFi.status() != WL_CONNECTED) {   Serial << '.'; delay(500); }
 
   Serial << "Connected to " << ssid << "IP address: " << WiFi.localIP() << endl;
+  Serial << "MAC Address is " << WiFi.macAddress() << endl;
 
-  broker.begin();
+  if (String(BROKER_MAC) == WiFi.macAddress()) startBroker = true;
 
-  mqtt_a.setCallback(onPublishA);
-  mqtt_a.subscribe(topic);
+  if(startBroker){
+    Serial << "Starting MDNS";
+    if (!MDNS.begin(BROKER_HOSTNAME)) {
+        Serial.println("Error setting up MDNS responder!");
+        while(1) {
+            delay(1000);
+        }
+    }
+    Serial.println("mDNS responder started");
 
-  mqtt_b.setCallback(onPublishB);
-  mqtt_b.subscribe(topic);
+    Serial << "STARTING BROKER" << endl;
+    broker.begin();
+
+    MDNS.addService("mqtt", "tcp", 1883);
+
+    mqtt_a.setCallback(onPublishA);
+    mqtt_a.subscribe(topic);
+
+    mqtt_b.setCallback(onPublishB);
+    mqtt_b.subscribe(topic);
+  }
+  else {
+    Serial << "Starting MDNS";
+    while(mdns_init()!= ESP_OK){ Serial << '.'; delay(1000); }
+
+    IPAddress serverIp;
+    Serial << "Resolving Broker IP";
+    while (serverIp.toString() == "0.0.0.0") {
+      Serial << '.';
+      serverIp = MDNS.queryHost(BROKER_HOSTNAME);
+      delay(500);
+    }
+    Serial << "Found broker with IP " << serverIp << endl;
+
+    int nrOfServices = MDNS.queryService("mqtt", "tcp");
+    if (nrOfServices == 0) {
+      Serial.println("No services were found.");
+    } 
+    else {
+     Serial << "Number of services found: " << nrOfServices << endl;
+      for (int i = 0; i < nrOfServices; i=i+1) {
+        Serial.println("---------------");
+        Serial << "Hostname: " << MDNS.hostname(i) << endl;
+        Serial << "IP: " << MDNS.IP(i) << endl;
+        Serial << "Port: " << MDNS.port(i) << endl;
+      }
+    }
+  }
+  
 }
 
 void loop()
 {
-  broker.loop();  // Don't forget to add loop for every broker and clients
+  if(startBroker){
 
-  mqtt_a.loop();
-  mqtt_b.loop();
+    broker.loop();  // Don't forget to add loop for every broker and clients
 
-  // ============= client A publish ================
-  static const int intervalA = 5000;  // publishes every 5s (please avoid usage of delay())
-  static uint32_t timerA = millis() + intervalA;
+    mqtt_a.loop();
+    mqtt_b.loop();
 
-  if (millis() > timerA)
-  {
-    Serial << "A is publishing " << topic.c_str() << endl;
-    timerA += intervalA;
-    mqtt_a.publish(topic, " sent by A");
-  }
+    // ============= client A publish ================
+    static const int intervalA = 5000;  // publishes every 5s (please avoid usage of delay())
+    static uint32_t timerA = millis() + intervalA;
 
-  // ============= client B publish ================
-  static const int intervalB = 7000;	// will send topic each 7s
-  static uint32_t timerB = millis() + intervalB;
+    if (millis() > timerA)
+    {
+      Serial << "A is publishing " << topic.c_str() << endl;
+      timerA += intervalA;
+      mqtt_a.publish(topic, " sent by A");
+    }
 
-  if (millis() > timerB)
-  {
-		static int temperature;
-    Serial << "B is publishing " << topic.c_str() << endl;
-    timerB += intervalB;
-    mqtt_b.publish(topic, " sent by B: "+std::string(String(16+temperature++%6).c_str()));
+    // ============= client B publish ================
+    static const int intervalB = 7000;	// will send topic each 7s
+    static uint32_t timerB = millis() + intervalB;
+
+    if (millis() > timerB)
+    {
+      static int temperature;
+      Serial << "B is publishing " << topic.c_str() << endl;
+      timerB += intervalB;
+      mqtt_b.publish(topic, " sent by B: "+std::string(String(16+temperature++%6).c_str()));
+    }
   }
 }
